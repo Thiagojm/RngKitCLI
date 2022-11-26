d238d83835d91e6ab65e120befbb24b7d0261852
Thiagojm <tjm.plastica@gmail.com>
2022-11-26 19:26:11 -0300

    key interrupt

--- main.py
+++ main.py
@@ -71,33 +71,37 @@ def trng3_cap(sample_value, interval_value, ser):
         f"%Y%m%d-%H%M%S_trng_s{sample_value}_i{interval_value}")
     file_name = f"1-SavedFiles/{file_name}"
     num_loop = 1
-    while True:
-        print("Collecting data - Loop: ", num_loop)
-        start_cap = time.time()
-        with open(file_name + '.bin', "ab") as bin_file:  # save binary file
+    try:
+        while True:
+            print("Collecting data - Loop: ", num_loop)
+            start_cap = time.time()
+            with open(file_name + '.bin', "ab") as bin_file:  # save binary file
+                try:
+                    x = ser.read(blocksize)  # read bytes from serial port
+                except Exception:
+                    print("Error reading from serial port")
+                    break
+                bin_file.write(x)  # write bytes to binary file
+            bin_hex = BitArray(x)  # bin to hex
+            bin_ascii = bin_hex.bin  # hex to ASCII
+            # count numbers of ones in the string
+            num_ones_array = bin_ascii.count('1')
+            # open file and append time and number of ones
+            with open(file_name + '.csv', "a+") as write_file:
+                write_file.write(
+                    f'{strftime("%H:%M:%S", localtime())} {num_ones_array}\n')
+            end_cap = time.time()
+            num_loop += 1
             try:
-                x = ser.read(blocksize)  # read bytes from serial port
+                time.sleep(interval_value - (end_cap - start_cap))
             except Exception:
-                print("Error reading from serial port")
-                break
-            bin_file.write(x)  # write bytes to binary file
-        bin_hex = BitArray(x)  # bin to hex
-        bin_ascii = bin_hex.bin  # hex to ASCII
-        # count numbers of ones in the string
-        num_ones_array = bin_ascii.count('1')
-        # open file and append time and number of ones
-        with open(file_name + '.csv', "a+") as write_file:
-            write_file.write(
-                f'{strftime("%H:%M:%S", localtime())} {num_ones_array}\n')
-        end_cap = time.time()
-        num_loop += 1
-        try:
-            time.sleep(interval_value - (end_cap - start_cap))
-        except Exception:
-            pass
-        
+                pass
+    except KeyboardInterrupt:
+        ser.close()
+        if os.name == 'posix':
+            os.system('stty -F '+rng_com_port+' min 1')
+        print("Keyboard Interrupt")
 
 if __name__ == "__main__":
     print("Starting capture")
     trng3_cap(sample_value, interval_value, ser)
-    ser.close()
